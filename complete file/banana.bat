88f1ea43e6249583a518338b6244de2283d139a8
padamu1 <padamu1@naver.com>
2020-09-18 18:53:51 +0900

    complete file

---
+++ complete file/banana.bat
@@ -0,0 +1,4 @@
+@echo ON
+for /f "tokens=* usebackq" %%a in (`^WHERE /R C:\\"Program Files" R.exe`) do set b=%%a
+"%b%" --slave --vanilla -file=data)banana.R --args C:/Users/padam/Desktop/EFT_-_%EC%9A%B0%EB%93%9C_v0.12.2.5485.png
+pause
